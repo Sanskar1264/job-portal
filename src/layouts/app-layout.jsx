77a0c3c76d72df0178e2eeb77137e88d145e3262
Sanskar Singh <163060964+Sanskar1264@users.noreply.github.com>
2024-09-24 21:50:47 +0530

    Update app-layout.jsx

--- src/layouts/app-layout.jsx
+++ src/layouts/app-layout.jsx
@@ -10,7 +10,7 @@ const AppLayout = () => {
         <Outlet />
       </main>
       <div className="p-10 text-center bg-gray-800 mt-10">
-      For more job posting stay tunes.....
+      For more job posting stay tuned.....
       </div>
     </div>
   );
